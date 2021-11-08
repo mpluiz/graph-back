7de2b87afbd8003db25b07bc6a5c1f8d6913d5fc
mpluiz <marcospaulo.luiz@gmail.com>
2021-11-07 22:57:45 -0300

    context configuration with api values

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs, resolvers } from './graphql/schema';
+import { context } from './graphql/context';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context
 });
 
 server.listen(3333).then(({ url }) => {
